5e36b6a887fdcb633b2b1348adaf68daa1e70484
Nico Vitt <nico.vitt@outlook.de>
2020-02-17 11:15:43 +0100

    Implemented some test-cases

--- examples/barbiegirl.ps1
+++ examples/barbiegirl.ps1
@@ -1,6 +1,5 @@
 # From https://github.com/EmpireProject/Empire/blob/master/lib/modules/powershell/trollsploit/thunderstruck.py
-Function Invoke-BarbieGirl
-{
+Function Invoke-BarbieGirl {
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory = $False, Position = 0)]
@@ -8,7 +7,7 @@ Function Invoke-BarbieGirl
         [String] $VideoURL = "https://youtu.be/ZyhrYis509A?t=13"
     )
     
-    Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}}
+    Function Set-Speaker($Volume) { $wshShell = new-object -com wscript.shell; 1..50 | % { $wshShell.SendKeys([char]174) }; 1..$Volume | % { $wshShell.SendKeys([char]175) } }
     Set-Speaker -Volume 50   
     #Create hidden IE Com Object
     $IEComObject = New-Object -com "InternetExplorer.Application"
@@ -16,10 +15,11 @@ Function Invoke-BarbieGirl
     $IEComObject.navigate($VideoURL)
     Start-Sleep -s 5
     $EndTime = (Get-Date).addseconds(10)
-    # ghetto way to do this but it basically presses volume up to raise volume in a loop for 90 seconds
-    do {
-       $WscriptObject = New-Object -com wscript.shell
-       $WscriptObject.SendKeys([char]175)
-    }
-    until ((Get-Date) -gt $EndTime)
+    # ghetto way to do this but it basically presses volume up to raise volume in a loop for 10 seconds
+    # do {
+    #     $WscriptObject = New-Object -com wscript.shell
+    #     $WscriptObject.SendKeys([char]175)
+    # }
+    # until ((Get-Date) -gt $EndTime)
+    $IEComObject.Quit();
 } Invoke-BarbieGirl
